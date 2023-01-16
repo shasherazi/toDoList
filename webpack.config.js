c7a274c3d4a58554c268040a384fc0f20c62b579
shasherazi <hassanrandomz@gmail.com>
2023-01-17 00:17:59 +0500

    Fix eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-unresolved
 
 module.exports = {
   entry: './src/index.js',
