f94fdeaedbc9642306150d7a541def975f9eaffb
shasherazi <hassanrandomz@gmail.com>
2023-01-17 14:20:48 +0500

    Add add functionaly to todo list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import renderToDoList from './modules/renderToDoList.js';
+import { renderToDoList, addTask } from './modules/listUtils.js';
 import './styles/style.css';
 
-const toDoTasks = [
-  { id: 1, task: 'Learn HTML', completed: true },
-  { id: 2, task: 'Learn JavaScript', completed: true },
-  { id: 3, task: 'Learn React', completed: false },
-];
+const input = document.querySelector('.task-input');
+const todoList = document.querySelector('.todo-list-ul');
 
+const toDoTasks = [];
 renderToDoList(toDoTasks);
+
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    addTask(toDoTasks, input.value);
+    input.value = '';
+  }
+});
+
+todoList.addEventListener('click', (e) => {
+  if (e.target.closest('.todo-list-li-checkbox')) {
+    const clickedCheckbox = e.target.closest('.todo-list-li-checkbox');
+    const clickedTask = clickedCheckbox.nextElementSibling;
+    const taskIndex = toDoTasks.findIndex((task) => task.task === clickedTask.textContent);
+    toDoTasks[taskIndex].completed = !toDoTasks[taskIndex].completed;
+    renderToDoList(toDoTasks);
+  }
+});
--- src/modules/listUtils.js
+++ src/modules/listUtils.js
@@ -1,17 +1,20 @@
 const toDoList = document.querySelector('.todo-list-ul');
 
-const renderToDoList = (toDoListArray) => {
+export const renderToDoList = (toDoListArray) => {
   toDoList.innerHTML = '';
 
   toDoListArray.forEach((toDo) => {
     const toDoItem = document.createElement('li');
     toDoItem.classList.add('todo-list-li');
     toDoItem.innerHTML = `
-        <input type="checkbox" class="todo-list-li-checkbox" ${toDo.completed ? 'checked' : ''}>
+    <input type="checkbox" class="todo-list-li-checkbox" ${toDo.completed ? 'checked' : ''}>
         <span class="todo-list-li-task">${toDo.task}</span>
         `;
     toDoList.appendChild(toDoItem);
   });
 };
 
-export default renderToDoList;
+export const addTask = (toDoListArray, task) => {
+  toDoListArray.push({ task, completed: false, id: toDoListArray.length + 1 });
+  renderToDoList(toDoListArray);
+};
