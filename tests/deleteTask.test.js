4343e90c97f8404d035ab6b2ce78b45989f9bec8
obed300 <obamfo@st.ug.edu.gh>
2023-01-26 10:12:54 +0000

    Test delete function

--- tests/deleteTask.test.js
+++ tests/deleteTask.test.js
@@ -19,4 +19,46 @@ describe('deleting tasks', () => {
 
     expect(toDOTasks).toEqual([]);
   });
+
+  test('deleting middle task', () => {
+    const toDoTasks = [
+      {
+        task: 'task1',
+        completed: false,
+        id: 1,
+      },
+      {
+        task: 'task2',
+        completed: false,
+        id: 2,
+      },
+      {
+        task: 'task3',
+        completed: false,
+        id: 3,
+      },
+    ];
+
+    const task = {
+      task: 'task2',
+      completed: true,
+      id: 2,
+    };
+
+    deleteTask(toDoTasks, task);
+
+    expect(toDoTasks).toEqual([
+      {
+        task: 'task1',
+        completed: false,
+        id: 1,
+      },
+
+      {
+        task: 'task3',
+        completed: false,
+        id: 2,
+      },
+    ]);
+  });
 });
