bfe04bf110605d30d7a8ead469f9d7be86d238c1
shasherazi <hassanrandomz@gmail.com>
2023-01-26 14:49:44 +0500

    Add tests for addition and deletion

--- tests/deleteTask.test.js
+++ tests/deleteTask.test.js
@@ -3,17 +3,9 @@ import { deleteTask } from '../src/modules/listUtils.js';
 describe('deleting tasks', () => {
   test('deleteTask', () => {
     const toDOTasks = [
-      {
-        task: 'task',
-        completed: false,
-        id: 1,
-      },
+      { task: 'task', completed: false, id: 1 },
     ];
-    const task = {
-      task: 'task',
-      completed: false,
-      id: 1,
-    };
+    const task = { task: 'task', completed: false, id: 1 };
 
     deleteTask(toDOTasks, task);
 
